993f45794b6c3fb9b657f22cdb8e77bfffa177d2
henry2craftman <henry2craftman@gmail.com>
2025-12-17 15:44:17 +0900

    feature: Audio, Particle, Navigation 예제
    - AudioStudy, ParticleStudy, NavicationStudy 씬, 스크립트 추가

---
+++ Assets/251216/AudioManager.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 1버튼을 누르면 BGM 재생, 2버튼을 누르면 punch1 재생
+/// 속성: AudioSource, audioclip들
+/// </summary>
+public class AudioManager : MonoBehaviour
+{
+    AudioSource audioSource;
+    [SerializeField] List<AudioClip> clips = new List<AudioClip>();
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            // BGM 재생
+            audioSource.clip = clips[0];
+            audioSource.Play();
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            // punch1 재생
+            audioSource.clip = clips[1];
+            audioSource.Play();
+        }
+
+
+    }
+}
---
+++ Assets/251217/ChaseTarget.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+/// <summary>
+/// 버튼을 누르면 Agent가 네비게이션매시의 특정 위치로 이동한다.
+/// 속성: 타겟 transform, NavMeshAgent
+/// </summary>
+public class ChaseTarget : MonoBehaviour
+{
+    [SerializeField] Transform target;
+    NavMeshAgent agent;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        agent.speed = 10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            agent.SetDestination(target.position);
+        }
+    }
+}
